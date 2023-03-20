5fe5de938b92b38ecbd0ec5621b4f2d05ec723a0
Maijs Garais <maireadsbooks@gmail.com>
2023-03-20 13:07:42 +0000

    add overlay with buttons to projectitems, add profile image to bio section

--- src/ContentContainer.jsx
+++ src/ContentContainer.jsx
@@ -1,5 +1,7 @@
 const ContentContainer = ({ children, className }) => {
-  return <div className={`w-4/5 mx-auto ${className}`}>{children}</div>;
+  return (
+    <div className={`w-4/5 md:w-3/5 mx-auto ${className}`}>{children}</div>
+  );
 };
 
 export default ContentContainer;
